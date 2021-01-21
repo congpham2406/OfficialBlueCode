420892a23ee5d7b2264756d35cdd0a64309eae69
congpham2406 <pbcong2014@gmail.com>
2021-01-22 00:08:12 +0700

    Update GoStraight.java

--- OfficialBlueCode/src/main/java/frc/robot/commands/GoStraight.java
+++ OfficialBlueCode/src/main/java/frc/robot/commands/GoStraight.java
@@ -22,7 +22,7 @@ public class GoStraight extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_drivebase.drive(-0.5, -0.4);
+    m_drivebase.drive(-0.5, -0.4);                                                        
   }
 
   // Called every time the scheduler runs while the command is scheduled.
